58cea4edebe7f1abbaa6eff8b774a8980ffd1f6e
Geordan Gutow <gmgutow@mtu.edu>
2026-09-04 10:16:13 -0400

    scalar q_factor

--- gemini-train_pi_groups.py
+++ gemini-train_pi_groups.py
@@ -93,7 +93,7 @@ def load_and_nondimensionalize_data(data_folder: str = 'data/experiment'):
             x_nondim = np.concatenate([pwm,reynolds_num, w_hat])
 
             # Non-dimensional force coefficient vector C_f
-            q_factor = 1#RHO * v_inf*v_inf * (ROTOR_RADIUS ** 2)
+            q_factor = RHO * v_inf*v_inf * (ROTOR_RADIUS ** 2)
             c_f = fa_local / q_factor
 
             X_list.append(x_nondim)
