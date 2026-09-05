46274edc192724d6c90ab30f6dcf7e1e890b4e27
Geordan Gutow <gmgutow@mtu.edu>
2026-09-05 13:23:57 -0400

    try 200 epochs for nondim as validation loss is still improving at 100

--- train_on_neural_fly_test_on_intel.py
+++ train_on_neural_fly_test_on_intel.py
@@ -1,3 +1,3 @@
 import gemini_train_pi_groups
-model,scaler_X,scaler_label=gemini_train_pi_groups.train_model(rotor_radius=gemini_train_pi_groups.ROTOR_RADIUS["neural-fly"],save_folder_prefix="train100epochs_and_save",data_folder="./data/training",epochs=100)
+model,scaler_X,scaler_label=gemini_train_pi_groups.train_model(rotor_radius=gemini_train_pi_groups.ROTOR_RADIUS["neural-fly"],save_folder_prefix="train100epochs_and_save",data_folder="./data/training",epochs=200)
 pred,rmse,percent=gemini_train_pi_groups.test_model_on_dataset(model,scaler_X,scaler_label,"./data/training-transfer/",gemini_train_pi_groups.ROTOR_RADIUS["intel"])
